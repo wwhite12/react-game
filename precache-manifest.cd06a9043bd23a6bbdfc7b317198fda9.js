7ea6c0e96f44a399e0d644028df225b6acd1e57d
William White <willcwhite@gmail.com>
2019-09-22 15:55:18 -0400

    Updates

---
+++ precache-manifest.cd06a9043bd23a6bbdfc7b317198fda9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cbff3cdb25ca21ef07048dc78ee2ffb",
+    "url": "/react-game/index.html"
+  },
+  {
+    "revision": "668230140ca9d6508088",
+    "url": "/react-game/static/css/main.1e14f9cb.chunk.css"
+  },
+  {
+    "revision": "ed4de29e4ba2e73cead5",
+    "url": "/react-game/static/js/2.8d936451.chunk.js"
+  },
+  {
+    "revision": "668230140ca9d6508088",
+    "url": "/react-game/static/js/main.0a489c77.chunk.js"
+  },
+  {
+    "revision": "f4abe13fae1af131a30f",
+    "url": "/react-game/static/js/runtime~main.067350e5.js"
+  }
+]);
